493c4b6f2944418852f9a7df07ff3581cb89068f
ulirodriguezz <ulirodrigueze@gmail.com>
2024-11-24 14:34:20 -0300

    Fixes al CORS e implentacion del S3 comentada

--- src/main/java/com/example/backenddesarrollodeapps2ecommerce/config/S3Config.java
+++ src/main/java/com/example/backenddesarrollodeapps2ecommerce/config/S3Config.java
@@ -1,15 +1,12 @@
 package com.example.backenddesarrollodeapps2ecommerce.config;
 
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
-    @Value("${s3region}")
-    private String region;
+
 /*    @Bean
     public AmazonS3 s3(){
         AWSCredentials awsCredentials = new BasicAWSCredentials("AKIAYRH5NBICBW7XSK2V","OjdZG7nmVCZZf6fSRMKNY6kPT/DdsD/NzmbZ8iqj");
